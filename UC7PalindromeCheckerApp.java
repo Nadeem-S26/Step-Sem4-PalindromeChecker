7b18abc38008dbff3a47c8eb8b819d72fd68ca86
Nadeem-S26 <nadeem26707@gmail.com>
2026-02-25 19:36:19 +0530

    UC7 Completed

---
+++ UC7PalindromeCheckerApp.java
@@ -0,0 +1,36 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
+public class UC7PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input: ");
+        String input = sc.nextLine();
+
+        
+        Deque<Character> deque = new ArrayDeque<>();
+
+        
+        for (char c : input.toCharArray()) {
+            deque.addLast(c);   
+        }
+
+        
+        boolean isPalindrome = true;
+
+        
+        while (deque.size() > 1) {
+
+            char front = deque.removeFirst();  
+            char rear = deque.removeLast();    
+
+            if (front != rear) {
+                isPalindrome = false;
+                break;
+            }
+        }
+        System.out.println("Is palindrome? : " + isPalindrome);
+    }
+}
